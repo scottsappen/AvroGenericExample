24328d22a681d787eb8daab3c3ba1d2fa8046b43
Scott Sappenfield <scotts@Scott-Sappenfield-MBP15.local>
2019-01-27 18:44:32 -0500

    Commit required files

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # AvroGenericExample
 Simple example that show how you can create an Avro object from a schema
+
+Example will create a generic record as an example using a rock climbing facility as the entity.
+
+This is a Java/Maven project using JDK1.8.
+
+Simple steps for a simple example:
+- Write the schema in the code (not recommended, but easy to read)
+- Create an object using that schema
+- Write out a file using that schema (this will create a .avro file)
+- Read the .avro file
+
+Common compile time errors
+
+Exception in thread "main" org.apache.avro.AvroRuntimeException: Field location type:STRING pos:2 not set and has no default value
+- means you probably did not set a required field
+
+Exception in thread "main" java.lang.NullPointerException
+- means you probably did something else wrong, maybe setting a field that does not exist in the schema
+
---
+++ src/main/java/com/github/scotts/avro/GenericRecordClimber.java
@@ -0,0 +1,82 @@
+package com.github.scotts.avro;
+
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.generic.*;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+
+import java.io.File;
+import java.io.IOException;
+
+public class GenericRecordClimber {
+
+    public static void main(String[] args) {
+
+        /**
+         * Simple steps for a simple example:
+         * - Write the schema in the code (not recommended, but easy to read)
+         * - Create an object using that schema
+         * - Write out a file using that schema (this will create a .avro file)
+         * - Read the .avro file
+         */
+
+        //let's define our schema in the code here
+        Schema.Parser parser = new Schema.Parser();
+        Schema schema = parser.parse("{\n" +
+                "     \"type\": \"record\",\n" +
+                "     \"namespace\": \"com.github.scotts.avro\",\n" +
+                "     \"name\": \"ClimbingGym\",\n" +
+                "     \"doc\": \"Avro Schema for our Climbing Gym\",\n" +
+                "     \"fields\": [\n" +
+                "       { \"name\": \"gym_name\", \"type\": \"string\", \"doc\": \"Name of Climbing Gym\" },\n" +
+                "       { \"name\": \"gym_nickname\", \"type\": \"string\", \"doc\": \"Nick Name of Climbing Gym\" },\n" +
+                "       { \"name\": \"location\", \"type\": \"string\", \"doc\": \"Location of the Climbing Gym\" },\n" +
+                "       { \"name\": \"hastopropeclimbing\", \"type\": \"boolean\", \"default\": true, \"doc\": \"Field indicating if the climbing gym offers top rope climbing\" },\n" +
+                "       { \"name\": \"hasleadclimbing\", \"type\": \"boolean\", \"default\": false, \"doc\": \"Field indicating if the climbing gym offers lead climbing\" },\n" +
+                "       { \"name\": \"hasbouldering\", \"type\": \"boolean\", \"default\": true, \"doc\": \"Field indicating if the climbing gym offers bouldering\" },\n" +
+                "       { \"name\": \"hasspeedclimbing\", \"type\": \"boolean\", \"default\": false, \"doc\": \"Field indicating if the climbing gym offers speed climbing\" }\n" +
+                "     ]\n" +
+                "}");
+
+        //let's create a generic record from our schema
+        GenericRecordBuilder climbingGymBuilder = new GenericRecordBuilder(schema);
+        climbingGymBuilder.set("gym_name", "Inner Peaks");
+        climbingGymBuilder.set("gym_nickname", "New Gym or South End");
+        climbingGymBuilder.set("location", "Charlotte, NC");
+        climbingGymBuilder.set("hasleadclimbing", true);
+        climbingGymBuilder.set("hasspeedclimbing", true);
+        GenericData.Record climbingGym = climbingGymBuilder.build();
+
+        //let's create a file for that record
+        final DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<>(schema);
+        try (DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<>(datumWriter)) {
+            dataFileWriter.create(climbingGym.getSchema(), new File("climbingGym-generic.avro"));
+            dataFileWriter.append(climbingGym);
+            System.out.println("Written climbingGym-generic.avro");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //let's read back that newly generated avro file, create a generic record and thumb through some fields
+        final File file = new File("climbingGym-generic.avro");
+        final DatumReader<GenericRecord> datumReader = new GenericDatumReader<>();
+        GenericRecord customerRead;
+        try (DataFileReader<GenericRecord> dataFileReader = new DataFileReader<>(file, datumReader)){
+            customerRead = dataFileReader.next();
+            System.out.println("Name of Climbing Gym: " + customerRead.get("gym_name"));
+            System.out.println("Nick Name of Climbing Gym: " + customerRead.get("gym_nickname"));
+            System.out.println("Location of the Climbing Gym: " + customerRead.get("location"));
+            System.out.println("Gym offers top rope climbing?: " + customerRead.get("hastopropeclimbing"));
+            System.out.println("Gym offers lead climbing: " + customerRead.get("hasleadclimbing"));
+            System.out.println("Gym offers bouldering: " + customerRead.get("hasbouldering"));
+            System.out.println("Gym offers speed climbing: " + customerRead.get("hasspeedclimbing"));
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
